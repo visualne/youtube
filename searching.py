13c6cfe1f1de3c6f195deb6aef021e569f448410
none <none@MacBook-Air.local>
2017-11-24 01:02:25 -0500

    gotta fix the amount of days in various months throughout the year

--- searching.py
+++ searching.py
@@ -8,6 +8,10 @@ import time
 from datetime import datetime
 
 #ToDo
+# Change publishedAfter and publishedBefore timestamps so you are searching everyday
+# for one year.
+#Print the timestamps
+
 # Finish changeDate function. Figure out how to create datetime object
 
 
@@ -31,53 +35,70 @@ def changeDate(options):
   # Midnight EST on 8-5-2008
   # 2008-08-04T04:00:00.000
 
-  publishedAfter='2016-08-04T00:00:00Z'
-  publishedBefore='2016-08-04T00:00:00Z'
-
   # Loop to create every hour in the day in GMT.
-  for month in range(1,13):
-    for day in range(1,32):
-      for hour in range(24):
+  for month in range(2,13):
+    # Loop to create every day in GMT.
+    for day in range(1,31):
+      # Loop to create ever hour in the day in GMT.
+      for hour in range(23,24):
+
+
+        # The below if statements need to be looked at. This is a weak attempt at dealing with months
+        # with 29 and 30 days in them.
+        if day + 1 == 30 and month == 2 and hour + 1 == 24:
+          break
+        # Dealing with 30 days in April
+        elif day + 1 == 31 and month == 4 and hour + 1 == 24:
+          break
+        # Dealing with 30 days in June
+        elif day + 1 == 31 and month == 6 and hours + 1 == 24:
+          break
+        # Dealing with 30 days in September
+        elif day + 1 == 31 and month == 9 and hours + 1 == 24:
+          break
+        # Dealing with 30 days in November
+        elif day + 1 == 31 and month == 11 and hours + 1 == 24:
+          break
+        else:
+          publishedAfter = '2016-' + str(month).zfill(2) + '-' + str(day).zfill(2) + 'T' + str(hour).zfill(2) + ':' + '00' + ':' + '00' + 'Z'
+
+          # Checking hour. If it is 23 we will break out of this for loop.
+          if hour == 23:
+            publishedBefore = '2016-' + str(month).zfill(2) + '-' + str(day + 1).zfill(2) + 'T' + '00' + ':' + '00' + ':' + '00' + 'Z'
+          else:
+            publishedBefore = '2016-' + str(month).zfill(2) + '-' + str(day).zfill(2) + 'T' + str(hour+1).zfill(2) + ':' + '00' + ':' + '00' + 'Z'
+
+
+          # Call to youtube api here with the search parameters.
+          youtube_search(options, publishedAfter, publishedBefore)
+
+
         # Loop to create every minute in the day in GMT.
-        for minute in range(60):
-          # for second in range(60):
-            # print '2016-'+str(month).zfill(2)+'-'+str(day).zfill(2)+'T'+str(hour).zfill(2) + ':' + str(minute).zfill(2) + ':' + str(second).zfill(2)
+        # for minute in range(60):
+          # for second in range(59):
+            # publishedAfter = '2016-' + str(month).zfill(2) + '-' + str(day).zfill(2) + 'T' + str(hour).zfill(2) + ':' + str(minute).zfill(2) + ':' + str(second).zfill(2) + 'Z'
+            # publishedBefore = '2016-' + str(month).zfill(2) + '-' + str(day).zfill(2) + 'T' + str(hour).zfill(2) + ':' + str(minute).zfill(2) + ':' + str(second+1).zfill(2) + 'Z'
 
-            # publishedAfter = '2016-'+str(month).zfill(2)+'-'+str(day).zfill(2)+'T'+str(hour).zfill(2) + ':' + str(minute).zfill(2) + ':' + str(second).zfill(2) + 'Z'
-            # publishedBefore = '2016-'+str(month).zfill(2)+'-'+str(day).zfill(2)+'T'+str(hour).zfill(2) + ':' + str(minute).zfill(2) + ':' + str(second + 2).zfill(2) + 'Z'
+            # Call to youtube api here with the search parameters.
+            # youtube_search(options,publishedAfter,publishedBefore)
 
-            # Creating published after and published before timestamps, every minute. I would like to ultimately check every SECOND
-            publishedAfter = '2016-'+str(month).zfill(2)+'-'+str(day).zfill(2)+'T'+str(hour).zfill(2) + ':' + str(minute).zfill(2) + ':00' + 'Z'
-            publishedBefore = '2016-'+str(month).zfill(2)+'-'+str(day).zfill(2)+'T'+str(hour).zfill(2) + ':' + str(minute + 30).zfill(2) + ':00' + 'Z'
-
-
-            print 'publishedAfter: ' + publishedAfter
-            print 'publishedBefore: ' + publishedBefore
-
-            # Calling youtube_search function with options, publishedAfter, publishedBefore
-            youtube_search(options, publishedAfter, publishedBefore)
-            # Sleeping for one second. This above api call with be executed...a lot
-            time.sleep(1)
-
-  # Calling search with the above date
-  # Need to search every hour every day for the whole day for a while year
-
-  # Search every month 1-12
-  # Search every hour 00:00 - 23:30
-  # Search every minute 00 - 59
+            # Sleeping for one second
+            # time.sleep(1)
 
 
 
-def youtube_search(options,publishedAfterTimestamp=None,publishedBeforeTimestamp=None):
+        # Leaving for loop when minute == 22
+
+def youtube_search(options,publishedAfterTimestamp=None,publishedBeforeTimestamp=None,nextPageToken=None):
   youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
     developerKey=DEVELOPER_KEY)
 
-  # print publishedAfterTimestamp
-  # print publishedBeforeTimestamp
-  # exit(1)
+  # Opening file handler
+  f = open('Results.txt','w+')
 
-  # Call the search.list method to retrieve results matching the specified
-  # query term.
+  # 2016-01-01T00:00:02Z
+
+  # Call the search.list method to retrieve results matching the specified query term.
   search_response = youtube.search().list(
     q=options.q,
     type='video',
@@ -89,24 +110,58 @@ def youtube_search(options,publishedAfterTimestamp=None,publishedBeforeTimestamp
 
   videos = []
 
-  # Add each result to the appropriate list, and then display the lists of
-  # matching videos, channels, and playlists.
-  for search_result in search_response.get("items", []):
-      videos.append("%s (%s) (%s)" % (search_result["snippet"]["title"],
-                                 search_result["id"]["videoId"],
-                                 search_result["snippet"]["channelId"]))
+  # Remove the below two lines
+  total_results = search_response['pageInfo']['totalResults']
+  next_page_token = search_response['nextPageToken']
+
+  # print 'Published After: ' + publishedAfterTimestamp + ' ' + 'Published Before: ' + publishedBeforeTimestamp + ' Total Results: ' + str(total_results)
 
 
-  print "Videos:\n", "\n".join(videos), "\n"
+  # For loop to run through each new page worth of results
+  for pageNum in range(total_results):
+    if pageNum == 0:
+      # Dealing with results on first page only
+      for search_result in search_response.get("items", []):
+
+        # Putting all the video search results into a list.
+        videos.append("%s (%s) (%s) (%s) (%s)" % (search_result["snippet"]["title"],
+                                        search_result["id"]["videoId"],
+                                        search_result["snippet"]["channelId"],
+                                        publishedAfterTimestamp,
+                                        publishedBeforeTimestamp))
+
+      # Checking size of videos list. If it is zero that means that ZERO results were found. This
+      # is odd because the API is telling me that there are results......
+      if len(videos) == 0:
+        # breaking out of for loop
+        break
+      else:
+        # print 'Published After: ' + publishedAfterTimestamp + ' ' + 'Published Before: ' + publishedBeforeTimestamp + ' Total Results: ' + str(total_results)
+        # print "Videos:\n", "\n".join(videos), "\n"
+        print "\n".join(videos), "\n"
+        result = "Videos:\n", "\n".join(videos), "\n"
+        # Writting videos found to output file.
+        f.write("\n".join(videos).encode('utf-8'))
+
+      # # Output string listed below.
+      # outputString = "Videos:\n", "\n".join(videos), "\n"
+      #
+      # for result in videos:
+      #   f.write(result.encode('utf-8') + '\n')
+
+
+  # Closing output file.
+  f.close()
 
 
 if __name__ == "__main__":
-  argparser.add_argument("--q", help="Search term", default="Google")
+  argparser.add_argument("--q", help="Search term", default="nothing")
   argparser.add_argument("--max-results", help="Max results", default=50)
   args = argparser.parse_args()
 
   try:
     changeDate(args)
+    # changeDate(args)
     # youtube_search(args)
   except HttpError, e:
     print "An HTTP error %d occurred:\n%s" % (e.resp.status, e.content)
